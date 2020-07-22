418335298ce71eb695d9ce8304f77f6519fe1826
Matheus Paice <matheus.paice@ikatec.com.br>
2020-07-22 19:07:43 -0300

    fix:adiciona eslint e prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import "./styles/GlobalStyles.css";
+import './styles/GlobalStyles.css';
 
 const App: React.FC = () => {
-  return <h1> Welcome to react </h1>;
+    return <h1> Welcome to react </h1>;
 };
 
 export default App;
