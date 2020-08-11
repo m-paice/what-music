6b2e83667208b4f72ad4bcaa4b7712eb4bb84428
Matheus Paice <matheus.paice@ikatec.com.br>
2020-08-10 23:50:54 -0300

    fix(tsconfig):corrige ts condig

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+
+interface UserData {
+    id: string;
+    login: string;
+}
 
 import "./styles/GlobalStyles.css";
 
 const App: React.FC = () => {
+    const [data, setData] = useState<UserData[]>([]);
+
+    useEffect(() => {
+        const fecthData = async () => {
+            const response = await axios.get("https://api.github.com/users");
+
+            console.log(response);
+
+            setData(response.data);
+        };
+
+        fecthData();
+    }, []);
+
     return (
         <>
             <h1> Welcome to react </h1>
+
+            {data.map((user) => {
+                return (
+                    <div key={user.id}>
+                        {" "}
+                        <strong> {user.id}: </strong> {user.login}{" "}
+                    </div>
+                );
+            })}
         </>
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -5,56 +5,56 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = () => {
-  const env = dotenv.config().parsed;
+    const env = dotenv.config().parsed;
 
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
+    const envKeys = Object.keys(env).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(env[next]);
+        return prev;
+    }, {});
 
-  return {
-    entry: {
-      app: path.join(__dirname, "src", "index.tsx"),
-    },
-    target: "web",
-    resolve: {
-      extensions: [".ts", ".tsx", ".js"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts(x?)$/,
-          use: "ts-loader",
-          exclude: "/node_modules/",
+    return {
+        entry: {
+            app: path.join(__dirname, "src", "index.tsx"),
         },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+        target: "web",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.(png|j?g|svg|gif)?$/,
+                    use: "file-loader",
+                },
+            ],
         },
-        {
-          test: /\.(png|j?g|svg|gif)?$/,
-          use: "file-loader",
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
         },
-      ],
-    },
-    output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    performance: {
-      hints: false,
-      maxEntrypointSize: 512000,
-      maxAssetSize: 512000,
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.join(__dirname, "public", "index.html"),
-      }),
-      new webpack.DefinePlugin(envKeys),
-      new CleanWebpackPlugin(),
-    ],
-  };
+        output: {
+            filename: "bundle.js",
+            path: path.resolve(__dirname, "dist"),
+        },
+        performance: {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000,
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "public", "index.html"),
+            }),
+            new webpack.DefinePlugin(envKeys),
+            new CleanWebpackPlugin(),
+        ],
+    };
 };
