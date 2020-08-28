56ac544abb0505db645b7ae88819ef5e54510a1c
Matheus Paice <matheus.paice@ikatec.com.br>
2020-08-27 21:42:59 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import React, { useEffect, useState } from "react";
-
-import axios from "axios";
-import { useQuery } from "react-query";
-
-interface User {
-    id: string;
-    login: string;
-}
-
-interface Response {
-    data: User[];
-}
+import React from "react";
 
 import "./styles/GlobalStyles.css";
 
-const App: React.FC = () => {
-    const { isLoading, error, data } = useQuery<Response, any>("usersData", () =>
-        axios.get("https://api.github.com/users"),
-    );
+interface Props {}
 
-    return (
-        <>
-            <h1> Welcome to react </h1>
-
-            {data?.data.map((user) => (
-                <div key={user.id}>
-                    {" "}
-                    <strong> {user.id}: </strong> {user.login}{" "}
-                </div>
-            ))}
-        </>
-    );
+const App: React.FC<Props> = (props) => {
+    return <h1>Hello World</h1>;
 };
 
 export default App;
